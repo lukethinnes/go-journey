0c0fa7ee8af85db839ee1e578a6282bb317aaa50
Luke Thinnes <lukethinnes@yahoo.com>
2021-09-09 15:10:00 -0600

    Coded a struct and value receiver method

---
+++ 12_structs/go.mod
@@ -0,0 +1,3 @@
+module github.com/lukethinnes/go-journey/12_structs
+
+go 1.17
---
+++ 12_structs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//Person defined with struct
+type Person struct {
+	firstName string
+	lastName  string
+	city      string
+	gender    string
+	age       int
+}
+
+//Methods
+
+//Greeting method (VALUE RECEIVER)
+
+//Requires identifier then name of struct
+func (p Person) greet() string {
+	return "Hello, my name is " + p.firstName + " " + p.lastName + " and I am " + strconv.Itoa(p.age)
+}
+
+func main() {
+	//Initialize a person using struct
+	person1 := Person{
+		firstName: "Luke",
+		lastName:  "Thinnes",
+		city:      "Denver",
+		gender:    "M",
+		age:       27,
+	}
+	fmt.Println(person1)
+
+	//Get a single field using dot notation...
+	fmt.Println(person1.greet())
+}
